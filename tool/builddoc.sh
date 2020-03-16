aebdef35f2e6ab5bab303435b7b49dfaae2f7907
Erik Arvidsson <erik.arvidsson@gmail.com>
2020-03-16 14:52:11 -0700

    Update builddoc.sh
    To filter out more non Replicache related docs

--- tool/builddoc.sh
+++ tool/builddoc.sh
@@ -3,6 +3,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT=$DIR/../
 
 dartdoc \
---exclude 'dart:async,dart:collection,dart:convert,dart:core,dart:developer,dart:io,dart:isolate,dart:math,dart:typed_data,dart:ui' \
+--exclude 'dart:async,dart:collection,dart:convert,dart:core,dart:developer,dart:ffi,dart:html,dart:io,dart:isolate,dart:js,dart:js_util,dart:math,dart:typed_data,dart:ui' \
 --input $ROOT \
 --output $ROOT/doc --no-include-source
