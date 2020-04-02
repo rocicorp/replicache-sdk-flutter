07fc872549f7f3f6a734eda9c8ace8bf18c2d267
Aaron Boodman <aaron@aaronboodman.com>
2020-04-01 15:18:09 -1000

    Roll replicache-client

--- tool/build.sh
+++ tool/build.sh
@@ -2,7 +2,7 @@ ORIG=`pwd`
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT=$DIR/../
 REPM_REPO='rocicorp/replicache-client'
-REPM_VERSION='v1.0.0'
+REPM_VERSION='v0.8.0'
 PACKAGE_VERSION=`git describe --tags`
 
 echo "Building Flutter SDK..."
