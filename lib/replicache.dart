d93363ef42208e83361a79f63e3e1fd76cd1610d
Erik Arvidsson <arv@roci.dev>
2020-05-13 16:24:27 -0700

    Do an initial sync directly after open
    This regressed with 719372413c351320de3d071cd2cb54e13ed5d012

--- lib/replicache.dart
+++ lib/replicache.dart
@@ -148,7 +148,9 @@ class Replicache implements ReadTransaction {
     _opened = _staticInvoke(_name, 'open');
     _root = _getRoot();
     await _root;
-    _scheduleSync();
+    if (_syncInterval != null) {
+      await sync();
+    }
   }
 
   String get name => _name;
